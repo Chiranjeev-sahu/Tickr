c14755dbf87172350da83515f7928f3df486bde2
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2025-04-21 12:00:00 +0530

    wrote the models for the DB

---
+++ src/models/Todo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const TodoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      trim: true,
+      default: "others",
+    },
+    priority: {
+      type: String,
+      default: "medium",
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Todo = mongoose.model("Todo", "todos", TodoSchema);
+module.exports = Todo;
---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", "users", UserSchema);
+module.exports = User;
